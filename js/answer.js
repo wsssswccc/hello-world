f725e633d1873fb3ea03e2227bf472278ce18fb3
wsssswccc <2356277990@qq.com>
2019-02-17 00:00:10 +0800

    second time to commit

--- js/answer.js
+++ js/answer.js
@@ -3,9 +3,14 @@ var A = document.getElementById('A');
 var B = document.getElementById('B');
 var C = document.getElementById('C');
 var D = document.getElementById('D');
+var again = document.getElementById('again');
 var i = 1;
-var store = 0;
-var Next = function(){
+var score;
+var Again = function()  
+{  
+   window.location.reload();  
+}  
+var Next = function(x){
 			var request = new XMLHttpRequest();
 			request.onreadystatechange = function(){
 				if(request.status === 200 && request.readyState === 4) {
@@ -23,44 +28,89 @@ var Next = function(){
 			}
 			if(i == 1){
 				request.open("GET","https://easy-mock.com/mock/5c17b21e40c07229aa2e6061/example/first-question",true);
-				request.send();
+				request.send();	
+				// setTimeout("Next()",10000);
+				score = 0;
 			}
+			
 			else if(i == 2){
 				request.open("GET","https://easy-mock.com/mock/5c17b21e40c07229aa2e6061/example/second-question",true);
 				request.send();
+				// setTimeout("Next()",10000);
+				if (x === A) {
+						score = 10 + score;
+				}
 			}
 			else if(i == 3){
 				request.open("GET","https://easy-mock.com/mock/5c17b21e40c07229aa2e6061/example/third-question",true);
 				request.send();
+				// setTimeout("Next()",10000);
+				if (x === C) {
+						score = 10 + score;
+				}
 			}
 			else if(i == 4){
 				request.open("GET","https://easy-mock.com/mock/5c17b21e40c07229aa2e6061/example/forth-question",true);
 				request.send();
+				// setTimeout("Next()",10000);
+				if (x === D) {
+						score = 10 + score;
+					}
 			}
 			else if(i == 5){
 				request.open("GET","https://easy-mock.com/mock/5c17b21e40c07229aa2e6061/example/fifth-question",true);
 				request.send();
+				// setTimeout("Next()",10000);
+				if (x === D) {
+						score = 10 + score;
+					}
 			}
 			else if(i == 6){
 				request.open("GET","https://easy-mock.com/mock/5c17b21e40c07229aa2e6061/example/sixth-question",true);
 				request.send();
+				// setTimeout("Next()",10000);
+				if (x === B) {
+						score = 10 + score;
+					}
 			}
 			else if(i == 7){
 				request.open("GET","https://easy-mock.com/mock/5c17b21e40c07229aa2e6061/example/seventh-question",true);
 				request.send();
+				// setTimeout("Next()",10000);
+				if (x === A) {
+						score = 10 + score;
+					}
+
 			}
 			else if(i == 8){
 				request.open("GET","https://easy-mock.com/mock/5c17b21e40c07229aa2e6061/example/eighth-question",true);
 				request.send();
+				// setTimeout("Next()",10000);
+				if (x === C) {
+						score = 10 + score;
+					}
 			}
 			else if(i == 9){
 				request.open("GET","https://easy-mock.com/mock/5c17b21e40c07229aa2e6061/example/ninth-question",true);
 				request.send();
+				// setTimeout("Next()",10000);
+					if (x === B) {
+						score = 10 + score;
+					}
 			}
 			else if(i == 10){
 				request.open("GET","https://easy-mock.com/mock/5c17b21e40c07229aa2e6061/example/tenth-question",true);
-				request.send();
+				request.send();	
+				// setTimeout("Next()",10000);
+				if (x === D) {
+						score = 10 + score;
+					}	
 			}
-		i ++;
+			else if(i == 11){
+				score = 10 + score;
+				alert("你此次得分为" + score + " !! ");
+			}		
+														
+		i ++;				
 		return i;
 		}
